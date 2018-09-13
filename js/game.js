84690960b20caaf5f4d491c914241c425f876ebe
wangxiao0405 <1353052193@qq.com>
2018-09-13 22:30:40 +0800

    wx

--- js/game.js
+++ js/game.js
@@ -1,64 +1,67 @@
 class Game {
-    constructor(){
-        this.screen="";
-        this.letters=[];
-        this.sm=10;
-        this.fen=0;
-        this.fenbox="";
-        this.smbox="";
-        this.voice="";
-        this.alert="";
-        this.score="";
+    constructor() {
+        this.screen = "";
+        this.letters = [];
+        this.sm = 10;
+        this.fen = 0;
+        this.fenbox = "";
+        this.smbox = "";
+        this.voice = "";
+        this.alert = "";
+        this.score = "";
 
-}
-    createLetter(){
-        let letterBox=document.createElement("div");
-        letterBox.className="letterBox";
+    }
+
+    createLetter() {
+        let letterBox = document.createElement("div");
+        letterBox.className = "letterBox";
         let asc;
         let letter;
-        do{
-           asc=Math.floor(Math.random()*26+65);
-           letter=String.fromCharCode(asc);
-        }while (letterrepeat(letter,this.letters)) ;
-        letterBox.style.backgroundImage=`url(img/A_Z/${letter}.png)`
+        do {
+            asc = Math.floor(Math.random() * 26 + 65);
+            letter = String.fromCharCode(asc);
+        } while (letterrepeat(letter, this.letters)) ;
+        letterBox.style.backgroundImage = `url(img/A_Z/${letter}.png)`
 
         let left;
-        do{
-            left=Math.random()*(7.5-0.5*3)+0.5;
-        }while (leftrepeat(left,this.letters)) ;
-        let top=Math.random()*.72+.76;
-        let obj={};
-        let speed=Math.random()*0.05+0.05;
-        obj['top']=top;
-        obj['left']=left;
-        obj['node']=letterBox;
-        obj['name']=letter;
-        obj['speed']=speed;
+        do {
+            left = Math.random() * (7.5 - 0.5 * 3) + 0.5;
+        } while (leftrepeat(left, this.letters)) ;
+        let top = Math.random() * .72 + .76;
+        let obj = {};
+        let speed = Math.random() * 0.05 + 0.05;
+        obj['top'] = top;
+        obj['left'] = left;
+        obj['node'] = letterBox;
+        obj['name'] = letter;
+        obj['speed'] = speed;
 
         this.letters.push(obj);
 
 
-        letterBox.style.left=left+"rem";
-        letterBox.style.top=top+"rem";
+        letterBox.style.left = left + "rem";
+        letterBox.style.top = top + "rem";
 
         this.screen.appendChild(letterBox);
 
     };
+
     //下落
-    run(){
-        this.t=setInterval(()=>{
+    run() {
+        this.t = setInterval(() => {
             for (let item of this.letters) {
-                item['top']+=item['speed'];
-                if(item['top']>=7.18){
-                    this.remove(item['name'],1);
+                item['top'] += item['speed'];
+                if (item['top'] >= 7.18) {
+                    this.remove(item['name'], 1);
                     continue;
                 }
-                item['node'].style.top=item['top']+"rem";
+                item['node'].style.top = item['top'] + "rem";
             }
-        },100)
+        }, 100)
     }
+
     //消失
-    remove(letter,type) {
+    remove(letter, type) {
         for (let item of this.letters) {
             if (item['name'] == letter) {
 
@@ -79,15 +82,18 @@ class Game {
             }
         }
     }
+
     //停止
-    pause(){
+    pause() {
         clearInterval(this.t)
     }
-    death(){
+
+    death() {
         clearInterval(this.t);
-        this.alert.style.display="block";
-        this.score.innerText= this.fenbox.innerText;
+        this.alert.style.display = "block";
+        this.score.innerText = this.fenbox.innerText;
     }
+}
 //去重复
 function leftrepeat(left,letters){
     for (let item of letters) {
